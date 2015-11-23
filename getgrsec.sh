c4d943ae19a7067c817b47c65872702b511d43c4
David Sterba <dave@jikos.cz>
2015-11-23 10:32:25 +0100

    Update dowlnoad script
    Signed-off-by: David Sterba <dave@jikos.cz>

--- getgrsec.sh
+++ getgrsec.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
-ftp -V -o - "http://grsecurity.net/test.php" | grep grsecurity- | perl -p -e 's/href=/\n/g' | awk '/\.patch/ {print $1}' |egrep -v "\.sig|div|iptables" | sed 's/"//g' | while read URL
+[ -d grsecurity_patches ] && cd grsecurity_patches
+
+wget -c -O - "https://grsecurity.net/test.php" |
+	grep grsecurity- |
+	perl -p -e 's/href=/\n/g' |
+	awk '/\.patch/ {print $1}' |
+	egrep -v "\.sig|iptables" |
+	grep -v "restrict" |
+	sed -e 's/"//g' |
+	sed -e 's/>.*$//' |
+while read URL
 do
 	echo ${URL}
-	ftp -C -V "http://grsecurity.net/${URL}"
+	wget -nc "http://grsecurity.net/${URL}"
+	wget -nc "http://grsecurity.net/${URL}".sig
+
+	gpg --verify ${URL}.sig ${URL}
 done
-
-git add *
-git commit -a -C 6a0abdba5dd74430d661317c6cf7458a04a9348f
-git push
-
-
+mv changelog-test.txt changelog-test.txt.old
+wget -c http://grsecurity.net/changelog-test.txt
