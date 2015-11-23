4d7946bcdedc29f572e89258f042aa742e0ef040
David Sterba <dave@jikos.cz>
2015-11-23 10:34:28 +0100

    Add pax patches
    Signed-off-by: David Sterba <dave@jikos.cz>

---
+++ getpax.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+[ -d pax_patches ] && cd pax_patches
+
+if ! [ -z "$1" ]; then
+	wget -nc "https://grsecurity.net/~paxguy1/$1"
+	exit
+fi
+
+wget -c -O - "https://grsecurity.net/~paxguy1" |
+	perl -p -e 's/href=/\n/g' |
+	grep 'pax-linux-.*\.patch' |
+	sed -e 's/"//g' |
+	sed -e 's/>.*$//' |
+	tee paxlog.txt |
+	tail -n 1 |
+while read URL
+do
+	echo ${URL}
+	wget -nc "https://grsecurity.net/~paxguy1/${URL}"
+done
